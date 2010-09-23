9a2db5433282052d38dd8b35c23c1f4176536786
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2010-09-23 07:27:28 -0300

    Updated to last august update on original plugin. inserted a new logic for permit the webmaster not use section or not use catetories.

--- twitterpost.php
+++ twitterpost.php
@@ -15,7 +15,6 @@ class plgContentTwitterpost extends JPlugin
 	var $Password;
 	var $Tinyurl;
 	var $Maxlen;
-	var $post_edits;
 	/**
 	 * Constructor
 	 *
@@ -29,65 +28,63 @@ class plgContentTwitterpost extends JPlugin
 		parent::__construct($subject);
 
 		// load plugin parameters
+
 		$this->_plugin = JPluginHelper::getPlugin('content', 'twitterpost');
 		$this->params = new JParameter($this->_plugin->params);
 		$this->Username = $this->params->get('username');
 		$this->Password = $this->params->get('password');
 		$this->Tinyurl = (int) $this->params->get('tinyurl');
 		$this->Maxlen = (int) $this->params->get('maxlen');
-		$this->post_edits = (int) $this->params->get('post_edits');
 		}
 
 	/**
 	 * Plugin method with the same name as the event will be called automatically.
 	 */
 	function onAfterContentSave($article, $isNew)
-		{	
+		{
 		global $mainframe;
-	
-		if(($isNew || $this->post_edits) && $article->state)
+		//$isNew = true;
+		$categories = trim($this->params->get('categories'));
+		$sections = trim($this->params->get('sections'));
+		if ($categories)
 			{
-			$categories = trim($this->params->get('categories'));
-			$sections = trim($this->params->get('sections'));
-			if ($categories)
-				{
-				$ids_cat = explode(',', $categories);
-				JArrayHelper::toInteger($ids_cat);
-				}else{
-				$ids_cat=array();
-				}	
-			if ($sections)
-				{
-				$ids_sec = explode(',', $sections);
-				JArrayHelper::toInteger($ids_sec);
-				}else{
-				$ids_sec=array();
-				}
-			
-			if (!$sections || !$categories || in_array($article->catid, $ids_cat) || in_array($article->sectionid, $ids_sec) )
-				{
-				$link = JRoute::_(ContentHelperRoute::getArticleRoute($article->id, $article->catid, $article->sectionid));
-				$link = str_replace('//', '/', JURI::base() . $link);
-				$link = str_replace('http:/', 'http://', $link);
-				$link = str_replace('/administrator', '', $link);
-				if($this->params->get('tinyurl'))
-					{
-					$link = $this->make_tiny($link);
-					}
-				
-				if($this->params->get('maxlen'))
-					{
-					$article->title =substr($article->title,0,$this->params->get('maxlen'));
-					}
-				
-				$msg=$article->title . " - URL: " . $link;
-	
-				$this->postToTwitter($this->Username,$this->Password,$msg);
-	
-				}
-			return true;
+			$ids_cat = explode(',', $categories);
+			JArrayHelper::toInteger($ids_cat);
+			}else{
+			$ids_cat=array();
+			}
+
+
+		if ($sections)
+			{
+			$ids_sec = explode(',', $sections);
+			JArrayHelper::toInteger($ids_sec);
+			}else{
+			$ids_sec=array();
 			}
 		
+		if ((!$sections || ($sections && !$categories) || in_array($article->catid, $ids_cat) || in_array($article->sectionid, $ids_sec)) && $isNew)
+			{
+			$link = JRoute::_(ContentHelperRoute::getArticleRoute($article->id, $article->catid, $article->sectionid));
+			$link = str_replace('//', '/', JURI::base() . $link);
+			$link = str_replace('http:/', 'http://', $link);
+			$link = str_replace('/administrator', '', $link);
+			if($this->params->get('tinyurl'))
+				{
+				$link = $this->make_tiny($link);
+				}
+			
+			if($this->params->get('maxlen'))
+				{
+				$article->title =substr($article->title,0,$this->params->get('maxlen'));
+				}
+			
+			$msg=$article->title . " - URL: " . $link;
+
+			$this->postToTwitter($this->Username,$this->Password,$msg);
+
+			}
+		return true;
 		}
 
 	function make_tiny($url)
